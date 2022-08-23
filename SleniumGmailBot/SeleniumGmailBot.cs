f43348f0121b8e88885828644b222fbb10064f13
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-08-23 16:26:52 +0200

    Driver

--- SleniumGmailBot/SeleniumGmailBot.cs
+++ SleniumGmailBot/SeleniumGmailBot.cs
@@ -1,9 +1,13 @@
-﻿namespace SleniumGmailBot
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SleniumGmailBot
 {
     public class SeleniumGmailBot
     {
         static void Main(string[] args)
         {
+            IWebDriver driver = new ChromeDriver();
 
         }
     }
