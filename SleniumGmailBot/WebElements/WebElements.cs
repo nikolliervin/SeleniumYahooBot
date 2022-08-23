d1eb6f241478f91368760495f053e25e9eb22ec9
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-08-23 17:11:07 +0200

    Username Field Xpath

---
+++ SleniumGmailBot/WebElements/WebElements.cs
@@ -0,0 +1,9 @@
+﻿using OpenQA.Selenium;
+
+namespace SleniumGmailBot.WebElements
+{
+    public class WebElements
+    {
+        public By userName => By.XPath("//*[@id='identifierId']");
+    }
+}
