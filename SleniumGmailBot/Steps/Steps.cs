fc7cd56c14f4480d94662c9bfe507c6b76104df8
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-08-23 17:09:20 +0200

    Login Method

--- SleniumGmailBot/Steps/Steps.cs
+++ SleniumGmailBot/Steps/Steps.cs
@@ -10,5 +10,10 @@ namespace SleniumGmailBot.Steps
         {
             driver.Navigate().GoToUrl(url);
         }
+
+        public void Login(IWebDriver driver)
+        {
+
+        }
     }
 }
