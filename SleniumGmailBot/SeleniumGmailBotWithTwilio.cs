5579727e3af9d07d9fa5d90a51382ad037a51b04
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-08-26 19:39:11 +0200

    Storing all the emails in a List

--- SleniumGmailBot/SeleniumGmailBotWithTwilio.cs
+++ SleniumGmailBot/SeleniumGmailBotWithTwilio.cs
@@ -1,5 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using SleniumGmailBot;
+using System.Collections.Generic;
 
 namespace SleniumYahooBot
 {
@@ -11,11 +13,16 @@ namespace SleniumYahooBot
 
             IWebDriver driver = new ChromeDriver();
             Steps.Steps steps = new Steps.Steps();
+            IterateEmails iterate = new IterateEmails();
 
             steps.LandOnYahoo(driver, steps.URL);
             steps.Login(driver);
             steps.GoToMail(driver);
             steps.GetTheEmailOfIndex(driver, 3);
+
+            List<string> emails = new List<string>();
+
+            emails = iterate.IterateThroughEmails(driver);
 
 
 
