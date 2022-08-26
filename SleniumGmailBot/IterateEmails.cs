d884fee8e849aaef768d73032fc322870dc3aff2
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-08-26 19:36:08 +0200

    Iterating through emails class

---
+++ SleniumGmailBot/IterateEmails.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using System.Collections.Generic;
+
+namespace SleniumGmailBot
+{
+    public class IterateEmails : WebElements.WebElements
+    {
+
+        public List<string> IterateThroughEmails(IWebDriver driver)
+        {
+
+            List<string> emails = new List<string>();
+
+            for (int i = 3; i <= 5; i++)
+            {
+                emails.Add(driver.FindElement(YahooMails(i)).ToString());
+            }
+
+            return emails;
+
+        }
+
+    }
+}
