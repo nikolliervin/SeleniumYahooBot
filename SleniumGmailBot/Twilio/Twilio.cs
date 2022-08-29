2d22705574a8219044cc246db8f3f4f51e452f8c
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-08-29 12:38:03 +0200

    Accesing Emails in Twilio Class

--- SleniumGmailBot/Twilio/Twilio.cs
+++ SleniumGmailBot/Twilio/Twilio.cs
@@ -4,6 +4,11 @@ namespace SleniumGmailBot.Twilio
 {
     public class Twilio
     {
+        private readonly IterateEmails getEmails;
+        public Twilio(IterateEmails _getEmails)
+        {
+            getEmails = _getEmails;
+        }
         string accountSid = ConfigurationManager.AppSettings["TwilioAccountSid"];
         string accountAuthToken = ConfigurationManager.AppSettings["TwilioAccountAuthToken"];
 
